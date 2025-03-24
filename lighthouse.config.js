1ccc2f92c5dd219677da172feccb65fde733a8a2
John B. Roberts <johnbroberts@gmail.com>
2025-03-23 23:02:10 -0400

    adding github actions

---
+++ lighthouse.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: './dist',
+    },
+    assert: {
+      preset: 'lighthouse:recommended'
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+}; 
